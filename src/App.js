4afbaa342766bb3b0f9a2a0038845b3ea35c7ce4
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2024-01-15 14:02:12 +1100

    installed bootstrap, added containers, changed css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Weather App</p>
-      </header>
-      <footer>
-        {" "}
-        Coded by Lisbeth Stafferton ©2024 with some open-source{" "}
-        <em>
-          <a href="https://github.com/lisbethS4W0art9studio/react-app-weather-shecodes">
-            Github
-          </a>
-        </em>{" "}
-        code <em>Hosted with </em>
-        <a href="https://www.netlify.com">Netlify</a>{" "}
-      </footer>
+    <div className="App-container">
+      <h1>Weather App</h1>
+      <div className="container-info">
+        <footer>
+          {" "}
+          Coded by Lisbeth Stafferton ©2024 with some open-source{" "}
+          <em>
+            <a href="https://github.com/lisbethS4W0art9studio/react-app-weather-shecodes">
+              Github
+            </a>
+          </em>{" "}
+          code <em>Hosted with </em>
+          <a href="https://www.netlify.com">Netlify</a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
